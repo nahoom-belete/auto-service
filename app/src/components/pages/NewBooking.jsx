621473f6454765ea65e005934b23143f69b0495d
Nahoom Belete <nahoombelete24@gmail.com>
2025-01-12 12:51:55 +0200

    Change color themes

--- app/src/components/pages/NewBooking.jsx
+++ app/src/components/pages/NewBooking.jsx
@@ -2,24 +2,24 @@ import Navigation from "../partials/Navigation.jsx"
 
 function NewBooking() {
   return (
-    <div className="flex flex-col items-center gap-5">
-        <Navigation/>
-        <h1 className="text-2xl text-center">
+    <div className="flex flex-col items-center gap-5 h-screen font-sans">
+        {/* <Navigation/> */}
+        <h1 className="text-2xl text-center text-success">
             New Booking
         </h1>
-        <form className="flex flex-col gap-5">
-            <label className="font-bold mr-2" for="vehicle">Select the vehicle to service:</label>
-            <select id="vehicle" name="vehicle">
+        <form className="text-success flex flex-col gap-5 border-2  p-5 h-3/5 w-3/5 filter drop-shadow-lg">
+            <label for="vehicle">Select the vehicle to service:</label>
+            <select className="hover:filter hover:drop-shadow-lg text-gray-950" id="vehicle" name="vehicle">
                 <option value="polo">Polo</option>
                 <option value="bmw">BMW</option>
                 <option value="mercedes">Mercedes</option>
                 <option value="Nissan">Nissan</option>
                 <option value="suzuki">Suzuki</option>
             </select>
-            <label className="font-bold mr-2" for="vehicle">Select the date the service will take place:</label>
-            <input type="date"></input>
-            <label className="font-bold mr-2" for="vehicle">Select the type of service to be done:</label>
-            <select id="vehicle" name="vehicle">
+            <label for="vehicle">Select the date the service will take place:</label>
+            <input className="hover:filter hover:drop-shadow-lg text-gray-950" type="date"></input>
+            <label for="vehicle">Select the type of service to be done:</label>
+            <select className="hover:filter hover:drop-shadow-lg text-gray-950" id="vehicle" name="vehicle">
                 <option value="minor">Minor Service</option>
                 <option value="major">Major Service</option>
                 <option value="annual">Annual Service</option>
--- app/src/components/partials/NavLink.jsx
+++ app/src/components/partials/NavLink.jsx
@@ -1,7 +1,9 @@
-function NavLink({href, linkText }) {
+import "../../styles/App.css"
+
+function NavLink({href, linkText, textColor }) {
   return (
     <div>
-     <a href={href} className="rounded-sm bg-green-700 p-2 text-blogify-background font-bold font-mono">{linkText}</a>
+     <a href={href} className="text-primary hover:text-success rounded-sm p-2 text-blogify-background font-bold font-mono">{linkText}</a>
     </div>
   )
 }
