15401c8439973eea0279515b6ea9697415a55f08
Nahoom Belete <nahoombelete24@gmail.com>
2025-01-12 20:03:10 +0200

    Add bookings route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const { submitDetails } = require("../controllers/signUpLogic.js")
 
+//POST
 router.post('/register', submitDetails);
+
+router.post('/new-booking', addBoking);
 
 //GET
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const {getConnection, getCustomers, closeConnection}= require("./controllers/dbConnection.js")
 
 var routes = require('./routes/routes.js');
 
@@ -9,11 +8,6 @@ app.use(cors());
 app.use(express.json())
 
 app.use('/', routes);
-
-const dbInstance = getConnection();
-getCustomers(dbInstance);
-closeConnection(dbInstance);
-
 
 app.listen(8080, () => {
     console.log('server listening on port 8080')
