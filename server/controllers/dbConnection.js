847430427b2b1ed19151bdb6fd6a402dd9ed58fd
Nahoom Belete <nahoombelete24@gmail.com>
2025-01-12 18:16:13 +0200

    Add database connection

---
+++ server/controllers/dbConnection.js
@@ -0,0 +1,31 @@
+const oracledb = require('oracledb');
+
+const connStr = "localhost/xe";
+const usr = "auto_admin#s";
+const psw = "admin123";
+
+async function getConnection() {
+    const conn = await oracledb.getConnection(
+        {
+            user          : usr,
+            password      : psw, 
+            connectString : connStr
+        }
+    );
+
+    return conn
+}
+
+async function getCustomers(conn) {
+   
+    const connection = await conn;
+    const result = await connection.execute(`SELECT * FROM customer`);
+    console.log("Result is:", result.rows);
+}
+
+async function closeConnection(conn) {
+    const connection = await conn;
+    await connection.close();  
+}
+
+module.exports = { getConnection, getCustomers, closeConnection }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
+const {getConnection, getCustomers, closeConnection}= require("./controllers/dbConnection.js")
 
 var routes = require('./routes/routes.js');
 
@@ -8,6 +9,11 @@ app.use(cors());
 app.use(express.json())
 
 app.use('/', routes);
+
+const dbInstance = getConnection();
+getCustomers(dbInstance);
+closeConnection(dbInstance);
+
 
 app.listen(8080, () => {
     console.log('server listening on port 8080')
