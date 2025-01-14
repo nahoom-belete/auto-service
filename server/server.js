4b7cc7c83d3c031d4cfbde71d2560866345f3cf4
Nahoom Belete <nahoombelete24@gmail.com>
2025-01-14 02:23:15 +0200

    Add insertBooking function

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ app.use(express.json())
 
 app.use('/', routes);
 
-app.listen(1380, () => {
-    console.log('server listening on port 1380')
+app.listen(8080, () => {
+    console.log('server listening on port 8080')
 })
